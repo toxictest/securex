de4e7a6e7254407bc4feaf4bc4e0508c2441a526
Dr.X <156610908+toxictest@users.noreply.github.com>
2025-05-24 21:45:17 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HomePage from './App';
+ReactDOM.createRoot(document.getElementById('root')).render(<HomePage />);
